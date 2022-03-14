77174406262ff40a80c4b16c6a91c27d336cd42f
Andrioxaby <byandrioxa@gmail.com>
2022-03-14 14:51:59 +0300

    feat: add function fact

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
 /* ДОПИШИТЕ КОД ФУНКЦИЙ ЧТОБЫ ВСЕ РАБОТАЛО!*/
 
 /* Реализуйте функцию подсчета факториала числа n */
-function fact(){
-    let n = Number(factor.value);
-    let result;
-    /*
-    ВАШ КОД ТУТ
-    result =
-    */
-    resFact.innerHTML = result;
+function fact() {
+  let n = Number(factor.value);
+  let result = 1;
+  for (i = 1; i <= n; i++) {
+    result = result * i;
+  }
+  resFact.innerHTML = result;
 }
 
 /* Реализуйте функцию деления по модулю X на Y */
-function divByMod(){
-    let X = Number(devisibleMod.value);
-    let Y = Number(deviderMod.value);
-    let result;
-    /*
-    ВАШ КОД ТУТ
-    result =
-    */
-    resDivMode.innerHTML = result;
+function divByMod() {
+  let X = Number(devisibleMod.value);
+  let Y = Number(deviderMod.value);
+  let result;
+  /*
+  ВАШ КОД ТУТ
+  result =
+  */
+  resDivMode.innerHTML = result;
 }
 
 /* Реализуйте функцию подсчета дней */
-function computedDay(){
-    let N = Number(n_level.value);
-    let x = Number(x_for_day.value);
-    let result;
-    /*
-    ВАШ КОД ТУТ
-    while .../ for ...
-    выбирайте цикл
-    как посчитаете нужным
-    result =
-    */
-    resDayCount.innerHTML = result;
+function computedDay() {
+  let N = Number(n_level.value);
+  let x = Number(x_for_day.value);
+  let result;
+  /*
+  ВАШ КОД ТУТ
+  while .../ for ...
+  выбирайте цикл
+  как посчитаете нужным
+  result =
+  */
+  resDayCount.innerHTML = result;
 }
 
 /* Реализуйте функцию подсчета конвертированной суммы*/
 function convertation() {
-    // Используйте коэффициэнты
-    const UER_TO_DOL = 0.9;
-    const EUR_TO_RUB = 0.007;
-    const DOL_TO_EUR = 1.05;
-    const DOL_TO_RUB = 0.008;
-    const RUB_TO_DOL = 116;
-    const RUB_TO_EUR = 126;
+  // Используйте коэффициэнты
+  const UER_TO_DOL = 0.9;
+  const EUR_TO_RUB = 0.007;
+  const DOL_TO_EUR = 1.05;
+  const DOL_TO_RUB = 0.008;
+  const RUB_TO_DOL = 116;
+  const RUB_TO_EUR = 126;
 
-    // В следующих переменных записываем что выбрал/ввел пользователь
-    // конвертируемая сумма
-    let summ = Number(currCount.value)
-    // из какой валюты
-    let fir_curr = selectFirCurrency.value;
-    // в какую валюту
-    let sec_curr = selectSecCurrency.value;
+  // В следующих переменных записываем что выбрал/ввел пользователь
+  // конвертируемая сумма
+  let summ = Number(currCount.value)
+  // из какой валюты
+  let fir_curr = selectFirCurrency.value;
+  // в какую валюту
+  let sec_curr = selectSecCurrency.value;
 
-    let result;
-    /*
-    ВАШ КОД ТУТ
-    switch ()
-        case
-    result =
-    */
+  let result;
+  /*
+  ВАШ КОД ТУТ
+  switch ()
+      case
+  result =
+  */
 
-    resCurr.innerHTML = result;
+  resCurr.innerHTML = result;
 }
