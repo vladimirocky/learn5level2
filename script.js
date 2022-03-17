48f90e3645f9ac35f22296d9548a80d67efc756c
Ken Karlson <javarecords@mail.ru>
2022-03-17 11:27:18 +0300

    add convert

--- script.js
+++ script.js
@@ -9,22 +9,11 @@ function fact() {
     }
     resFact.innerHTML = result;
 }
-
-
-
-/* Реализуйте функцию деления по модулю X на Y */
-function divByMod() {
-    let X = Number(devisibleMod.value);
-    let Y = Number(deviderMod.value);
-    let result;
-    result = X % Y;
-    resDivMode.innerHTML = result;
-}
-
 /* Реализуйте функцию подсчета дней */
-function computedDay() {
+function computedDay(){
     let N = Number(n_level.value);
     let x = Number(x_for_day.value);
+    //let result = N * (x-1) +1 (^2);
     let result;
     /*
     ВАШ КОД ТУТ
@@ -39,28 +28,54 @@ function computedDay() {
 /* Реализуйте функцию подсчета конвертированной суммы*/
 function convertation() {
     // Используйте коэффициэнты
-    const UER_TO_DOL = 0.9;
+    const EUR_TO_DOL = 0.9;
     const EUR_TO_RUB = 0.007;
+  
     const DOL_TO_EUR = 1.05;
     const DOL_TO_RUB = 0.008;
+  
     const RUB_TO_DOL = 116;
     const RUB_TO_EUR = 126;
-
+  
     // В следующих переменных записываем что выбрал/ввел пользователь
+  
     // конвертируемая сумма
     let summ = Number(currCount.value)
+  
     // из какой валюты
-    let fir_curr = selectFirCurrency.value;
+    let fir_curr = String(selectFirCurrency.value);
+  
     // в какую валюту
     let sec_curr = selectSecCurrency.value;
-
+  
     let result;
-    /*
-    ВАШ КОД ТУТ
-    switch ()
-        case
-    result =
-    */
-
+  
+    let curs = fir_curr + sec_curr;
+  
+    console.log(curs);
+  
+    switch (curs) {
+      case 'eurodollar':
+        result = (summ / EUR_TO_DOL);
+        break;
+      case 'euroruble':
+        result = (summ / EUR_TO_RUB);
+        break;
+      case 'dollareuro':
+        result = (summ / DOL_TO_EUR);
+        break;
+      case 'dollarruble':
+        result = (summ / DOL_TO_RUB);
+        break;
+      case 'rubleeuro':
+        result = (summ / RUB_TO_EUR);
+        break;
+      case 'rubledollar':
+        result = (summ / RUB_TO_DOL);
+        break;
+      default:
+        result = 'Incorrect input';  
+    }
+  
     resCurr.innerHTML = result;
-}
+  }
