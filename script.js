2ba5cc7bb89b37aca0458d40c381bc3a3c5aa3f9
Roma2351 <95650797+Roma2351@users.noreply.github.com>
2022-04-10 21:28:05 +0400

    complete

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 /* ДОПИШИТЕ КОД ФУНКЦИЙ ЧТОБЫ ВСЕ РАБОТАЛО!*/
 
 /* Реализуйте функцию подсчета факториала числа n */
-function fact() {
+function fact(){
     let n = Number(factor.value);
-    let result;
+    let result = 1
+    /*
+    ВАШ КОД ТУТ
+    result =
+    */
+for(let i = n; i>0; i--){
+   result = result*i
+}
     
-    function factorial(n) {
-        if (n != 1) {
-            n = n * factorial(n-1);
-        } else {
-            n = 1;
-        }
-    return n;
-    }
-    
-    result = factorial(n);
-
     resFact.innerHTML = result;
 }
 
@@ -24,9 +20,13 @@ function divByMod(){
     let X = Number(devisibleMod.value);
     let Y = Number(deviderMod.value);
     let result;
-    
+    /*
+    ВАШ КОД ТУТ
+    result =
+    */
+
     result = X % Y;
-    
+
     resDivMode.innerHTML = result;
 }
 
@@ -34,20 +34,22 @@ function divByMod(){
 function computedDay(){
     let N = Number(n_level.value);
     let x = Number(x_for_day.value);
-    let result = 0;
+    let result = 0
+    let level = 0
+    while(true){
+        result++;
+        level+= x
+        if(level >= N) break;
 
-    let counter = 1 
-
-    if (x < 2) {
-        
-    result = "ошибка";
-
-    } else {
-    while (counter * x - (counter - 1) < N) {
-      counter++
+        level--;
     }
-    result = counter
-  }
+    /*
+    ВАШ КОД ТУТ
+    while .../ for ...
+    выбирайте цикл
+    как посчитаете нужным
+    result =
+    */
     resDayCount.innerHTML = result;
 }
 
@@ -60,42 +62,57 @@ function convertation() {
     const DOL_TO_RUB = 0.008;
     const RUB_TO_DOL = 116;
     const RUB_TO_EUR = 126;
-
+    
     // В следующих переменных записываем что выбрал/ввел пользователь
     // конвертируемая сумма
-    let summ = Number(currCount.value);
+    let summ = Number(currCount.value)
     // из какой валюты
     let fir_curr = selectFirCurrency.value;
     // в какую валюту
     let sec_curr = selectSecCurrency.value;
-
+    
     let result;
     
-    let curs = fir_curr.concat(sec_curr);
+    /*
+    ВАШ КОД ТУТ
+    switch ()
+        case
+    result =
+    */
 
+    let curs = fir_curr + sec_curr;
+    
     switch (curs) {
         case '”euro””dollar”':
-            result = (summ / EUR_TO_DOL);
-            break;
+           result = summ / EUR_TO_DOL;
+           break;
+
         case '”euro””ruble”':
-            result = (summ / EUR_TO_RUB);
+            result = summ / EUR_TO_RUB;
             break;
+        
         case '”dollar””euro”':
-            result = (summ / DOL_TO_EUR);
+            result = summ / DOL_TO_EUR;
             break;
+
         case '”dollar””ruble”':
-            result = (summ / DOL_TO_RUB);
+            result = summ / DOL_TO_RUB ;
             break;
+
         case '”ruble””euro”':
-            result = (summ / RUB_TO_EUR);
+            result = summ / RUB_TO_EUR;
             break;
+        
         case '”ruble””dollar”':
-            result = (summ / RUB_TO_DOL);
+            result = summ / RUB_TO_DOL;
             break;
+        
         default:
-            result = 'Всё поломалось(';
-    }
+            result = 'Операция не возможна!!!'
+           alert("Не возможно конвертировать одну и туже валюту");
+           
+     
+    };
 
     resCurr.innerHTML = result;
-}
-
+};
