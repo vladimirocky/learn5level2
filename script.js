5a861742f098f8f3e63ffa86eaf3ed90283cb59a
Radli1 <i6100009@gmail.com>
2022-04-19 12:12:54 +0400

    Radli Commit

--- script.js
+++ script.js
@@ -3,11 +3,10 @@
 /* Реализуйте функцию подсчета факториала числа n */
 function fact(){
     let n = Number(factor.value);
-    let result;
-    /*
-    ВАШ КОД ТУТ
-    result =
-    */
+    result = 1;
+    for (let i = n; i > 0; i--) {
+        result = result * i;}
+
     resFact.innerHTML = result;
 }
 
@@ -15,19 +14,27 @@ function fact(){
 function divByMod(){
     let X = Number(devisibleMod.value);
     let Y = Number(deviderMod.value);
-    let result;
-    /*
-    ВАШ КОД ТУТ
-    result =
-    */
+    result = X % Y;
     resDivMode.innerHTML = result;
 }
 
 /* Реализуйте функцию подсчета дней */
-function computedDay(){
+function computedDay() {
     let N = Number(n_level.value);
     let x = Number(x_for_day.value);
-    let result;
+    let level = 0;
+    let result = 0;
+    while (true) {
+        result ++;
+        level += x;
+        if (level >= N) break;
+        level--;
+        }
+        resDayCount.innerHTML = result;
+}
+
+
+
     /*
     ВАШ КОД ТУТ
     while .../ for ...
@@ -35,13 +42,13 @@ function computedDay(){
     как посчитаете нужным
     result =
     */
-    resDayCount.innerHTML = result;
-}
+
+
 
 /* Реализуйте функцию подсчета конвертированной суммы*/
 function convertation() {
     // Используйте коэффициэнты
-    const UER_TO_DOL = 0.9;
+    const EUR_TO_DOL = 0.9;
     const EUR_TO_RUB = 0.007;
     const DOL_TO_EUR = 1.05;
     const DOL_TO_RUB = 0.008;
@@ -50,19 +57,33 @@ function convertation() {
 
     // В следующих переменных записываем что выбрал/ввел пользователь
     // конвертируемая сумма
-    let summ = Number(currCount.value)
+    let summ = Number(currCount.value);
     // из какой валюты
-    let fir_curr = selectFirCurrency.value;
+    let fir_curr = String(selectFirCurrency.value);
     // в какую валюту
-    let sec_curr = selectSecCurrency.value;
-
+    let sec_curr = String(selectSecCurrency.value);
     let result;
-    /*
-    ВАШ КОД ТУТ
-    switch ()
-        case
-    result =
-    */
+    let curs = fir_curr + sec_curr;
+    switch (curs) {
+        case 'eurodollar':
+            result = (summ / EUR_TO_DOL);
+            break;
+        case 'euroruble':
+            result = (summ / EUR_TO_RUB);
+            break;
+        case 'dollareuro':
+            result = (summ / DOL_TO_EUR);
+            break;
+        case 'dollarruble':
+            result = (summ / DOL_TO_RUB);
+            break;
+        case 'rubledollar':
+            result = (summ / RUB_TO_DOL);
+            break;
+        case 'rubleeuro':
+            result = (summ / RUB_TO_EUR);
+            break;
+    }
 
     resCurr.innerHTML = result;
 }
